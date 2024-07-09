358865975e7270d39b21a2a80d3ea3dbf93d2aa1
AWinkelman02 <awinkelman02@gmail.com>
2024-07-09 12:49:47 -0500

    Added routing for pulling data from database.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const controller = require("../controllers/controller")
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Flappybara' });
 });
+
+router.get('/data', controller.leaderboardData);
+
+router.post('/', controller.leaderboardPost);
 
 module.exports = router;
